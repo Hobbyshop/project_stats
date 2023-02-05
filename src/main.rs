ee4353b28b1a05a7073c3d5f8bc20e59a627cede
Hobbyshop <marc.deike@outlook.de>
2023-02-05 15:46:02 +0100

    get files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::{fs::{self, ReadDir, DirEntry}};
+
 fn main() {
-    println!("Hello, world!");
+    let target_dir = &std::env::args().collect::<Vec<String>>()[1];
+    let path = fs::read_dir(target_dir).unwrap();
+
+    for p in read_dir(path) {
+        println!("{}", p.path().display());
+    }
+}
+
+fn read_dir(dir: ReadDir) -> Vec<DirEntry> {
+    let mut files = Vec::<DirEntry>::new();
+
+    for path in dir {
+        let path = path.unwrap();
+
+        if path.metadata().unwrap().is_dir() {
+            let mut f = read_dir(path.path().read_dir().unwrap());
+            files.append(&mut f);
+            continue;
+        }
+        files.push(path);
+    }
+
+    files
 }
